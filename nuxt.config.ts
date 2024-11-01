16e12bbbafd1567d34fc890c50b4ef66025f7926
Mykyta Onishchenko <on11shchenko@gmail.com>
2024-11-01 17:25:42 +0200

    seo and icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,9 +6,68 @@ export default defineNuxtConfig({
       SECRET_KEY: process.env.SECRET_KEY,
     },
   },
-  css: ['~/assets/css/main.css'],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'Марафон Зцілення Душі: Сила Карми та Енергії',
+      meta: [
+        {
+          name: 'description',
+          content:
+            "Приєднуйтесь до марафону 'Зцілення Душі: Сила Карми та Енергії' з Оленою Оніщенко. Дізнайтеся, як знайти баланс між духовним та матеріальним, звільнитися від енергетичних блоків та посилити енергію своєї душі.",
+        },
+        {
+          name: 'keywords',
+          content:
+            'марафон, зцілення душі, карма, енергія, Олена Оніщенко, духовний баланс, медитація, вебінар',
+        },
+        {
+          property: 'og:title',
+          content: 'Марафон Зцілення Душі: Сила Карми та Енергії',
+        },
+        {
+          property: 'og:description',
+          content:
+            "Приєднуйтесь до марафону 'Зцілення Душі: Сила Карми та Енергії' з Оленою Оніщенко. Дізнайтеся, як знайти баланс між духовним та матеріальним, звільнитися від енергетичних блоків та посилити енергію своєї душі.",
+        },
+        {
+          property: 'og:image',
+          content: '@/assets/images/hero-image.png',
+        },
+        {
+          property: 'og:url',
+          content: 'https://neo18.com.ua',
+        },
+        {
+          name: 'twitter:title',
+          content: 'Марафон Зцілення Душі: Сила Карми та Енергії',
+        },
+        {
+          name: 'twitter:description',
+          content:
+            "Приєднуйтесь до марафону 'Зцілення Душі: Сила Карми та Енергії' з Оленою Оніщенко. Дізнайтеся, як знайти баланс між духовним та матеріальним, звільнитися від енергетичних блоків та посилити енергію своєї душі.",
+        },
+        {
+          name: 'twitter:image',
+          content: '@/assets/images/hero-image.png',
+        },
+        {
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        },
+      ],
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          sizes: '16x16',
+          href: '/favicon.svg',
+        },
+      ],
+    },
+  },
   modules: ['@pinia/nuxt'],
   plugins: ['~/plugins/scroll.ts'],
   pinia: {
